7e8f2cfd7a28fd6baf0ebc2821114226f7bf1466
DESKTOP-5CIN19L\糸永拓真 <k488453@kansai-u.ac.jp>
2026-01-18 15:23:06 +0900

    feat: ゲームシーンの作成と階層設定 (Coordinateの修正含む)

--- Assets/scripts/Backend/Models/PylosCoordinate.cs
+++ Assets/scripts/Backend/Models/PylosCoordinate.cs
@@ -1,16 +1,28 @@
+using System;
 using UnityEngine;
 
-[System.Serializable]
-public struct PylosCoordinate
+[Serializable]
+public class PylosCoordinate
 {
-    public int Level;
     public int X;
     public int Y;
 
-    public PylosCoordinate(int x, int y, int level)
+    //（Zを使う）
+    public int Z;
+
+    // （Levelを使う）
+    // 「Level」と呼ばれたら、自動的に「Z」の中身を返す！
+    public int Level
+    {
+        get { return Z; }
+        set { Z = value; }
+    }
+
+    // コンストラクタ
+    public PylosCoordinate(int x, int y, int z)
     {
         this.X = x;
         this.Y = y;
-        this.Level = level;
+        this.Z = z;
     }
 }
